ece1948cd0ac3d167ddf80866944dcbbff780132
Yihui Xiong <yihui_xiong@163.com>
2016-10-31 14:52:37 +0800

    initial

---
+++ setup.py
@@ -0,0 +1,44 @@
+from __future__ import unicode_literals
+
+import re
+
+from setuptools import find_packages, setup
+
+
+def get_version(filename):
+    with open(filename) as fh:
+        metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", fh.read()))
+        return metadata['version']
+
+
+setup(
+    name='Mopidy-FileManager',
+    version=get_version('mopidy_filemanager/__init__.py'),
+    url='https://github.com/xiongyihui/mopidy-filemanager',
+    license='Apache License, Version 2.0',
+    author='Yihui Xiong',
+    author_email='yihui.xiong@seeed.cc',
+    description='Mopidy extension for file manager',
+    long_description=open('README.rst').read(),
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'setuptools',
+        'Mopidy >= 1.0',
+        'Pykka >= 1.1',
+    ],
+    entry_points={
+        'mopidy.ext': [
+            'filemanager = mopidy_filemanager:Extension',
+        ],
+    },
+    classifiers=[
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+    ],
+)
